0b995de7404ac3238a965b3a431703828f085541
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-27 20:23:34 +0530

    Explore the tuple in python

---
+++ sorting/basic_data_types/tuple.py
@@ -0,0 +1,9 @@
+
+#  tupple like list but its not perform opertaions like appned, remove
+
+my_tuple = (1, 1.5, 'Tupple', True)
+print(type(my_tuple))
+print(len(my_tuple))
+
+for i in my_tuple:
+    print(i)
