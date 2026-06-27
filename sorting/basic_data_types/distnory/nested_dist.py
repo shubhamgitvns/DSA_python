1469547b037d706cef2aead4d6375cde37f8550c
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-27 21:04:45 +0530

    Nested dist

---
+++ sorting/basic_data_types/distnory/nested_dist.py
@@ -0,0 +1,17 @@
+students = {
+    "student1": {
+        "name": "Shubham",
+        "age": 22
+    },
+    "student2": {
+        "name": "Rahul",
+        "age": 23
+    }
+}
+
+students["student1"]["city"]='varanasi'
+students["student2"]["city"]='varanasi'
+
+print(students["student2"])
+
+print(students.items())
