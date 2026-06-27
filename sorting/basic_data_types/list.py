25a56049c294b88ff4183aeb77eeccd750f67659
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-27 20:06:48 +0530

    list data type in python

---
+++ sorting/basic_data_types/list.py
@@ -0,0 +1,9 @@
+# In the list sitore the differnt dat type elments and perform the opertations.
+
+my_list = [1, 1.9, "skm", False]
+
+print(my_list)
+my_list.append(5)
+print(my_list)
+my_list.remove(False)
+print(my_list)
--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,14 +1,22 @@
-arry=[7,4,6,-2,8]
+a=[1,2,3,6,-5,-9]
+min = a[0]
+minpos = 0
+n=len(a)
+for i in range(1,n):
+     if a[i]<min: # -5 < 1
+        min=a[i]
+        minpos=i
+        print(a)
+    
+# print(a)
+a[0],a[minpos]=a[minpos],a[0]
+print(a)
 
-n = len(arry)
-for i in range(n-1):
-    min_index = i
 
-    if min_index < i+1:
-       continue
-   
-arry[i],arry[min_index] = arry[min_index],arry[i]
 
-print(arry)
+
+
+
+
 
 
